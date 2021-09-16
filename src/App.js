c66819f14a4ec629f0e2fd34bccbe063012ff6e4
Daryl Nauman <darylnauman@gmail.com>
2021-09-16 15:25:21 -0400

    added cards of monsters

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: []
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            monsters: [],
+        };
+    }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}))
-  }
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.json())
+            .then((users) => this.setState({ monsters: users }));
+    }
 
-  render() {
-    return (
-      <div className="App">
-        {
-          this.state.monsters.map((monster) => <h1 key={monster.id}> {monster.name} </h1>)
-        }
-    </div>
-    )
-  }
+    render() {
+        return (
+            <div className="App">
+                <CardList monsters={this.state.monsters}/>
+            </div>
+        );
+    }
 }
 
 export default App;
