a9af3e90adea08682a0c2b8774a7427e0158a27b
Daryl Nauman <darylnauman@gmail.com>
2021-09-16 19:46:57 -0400

    added search functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
             monsters: [],
+            searchField: ''
         };
     }
 
@@ -17,11 +19,17 @@ class App extends Component {
     }
 
     render() {
-        return (
-            <div className="App">
-                <CardList monsters={this.state.monsters}/>
-            </div>
-        );
+      const { monsters, searchField } = this.state;
+      const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+      )
+
+      return (
+        <div className="App">
+          <SearchBox placeholder="Search monsters" handleChange={e => this.setState({searchField: e.target.value})}/>
+          <CardList monsters={filteredMonsters}/>
+        </div>
+      );
     }
 }
 
