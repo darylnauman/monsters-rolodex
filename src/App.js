d4745a4e7f69505b99d50e980d203a95df814638
Daryl Nauman <darylnauman@gmail.com>
2021-09-18 10:16:41 -0400

    additional styling

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ class App extends Component {
             .then((users) => this.setState({ monsters: users }));
     }
 
+    handleChange = (e) => {
+      this.setState({searchField: e.target.value})
+    }
+
     render() {
       const { monsters, searchField } = this.state;
       const filteredMonsters = monsters.filter(monster => 
@@ -26,7 +30,8 @@ class App extends Component {
 
       return (
         <div className="App">
-          <SearchBox placeholder="Search monsters" handleChange={e => this.setState({searchField: e.target.value})}/>
+          <h1>Monsters Rolodex</h1>
+          <SearchBox placeholder="Search monsters" handleChange={this.handleChange}/>
           <CardList monsters={filteredMonsters}/>
         </div>
       );
